68f9ac4ed5adac29097f42f9f164d88736ea51c7
setokun <s.guardo@live.fr>
2015-07-04 16:59:14 +0200

    Inclusion de la classe Boss au sein du thème Brambles

--- Winds/src/themes/brambles/Theme.java
+++ Winds/src/themes/brambles/Theme.java
@@ -15,7 +15,6 @@ import addon.ThemeBase;
 import annotation.wFiles;
 import annotation.wTheme;
 import core.Arrival;
-import core.Boss;
 import core.Collectable;
 import core.CollectableId;
 import core.CollisionBox;
@@ -361,4 +360,64 @@ public class Theme extends ThemeBase {
 		}
 	}
 
+	private static class Boss extends GameObject{
+		int count;
+		//private Animation animation;
+		
+		private static BufferedImage sprite;
+		private ArrayList<CollisionBox> collisions;
+		
+		static {
+			BufferedImageLoader loader = new BufferedImageLoader();
+			sprite = new SpriteSheet(loader.loadImage("/enemies/boss_bee.png"), 96).grabImage(0, 0);
+		}
+		
+		public Boss(float x, float y,ObjectId id) {
+			super(x, y, id);
+			
+			this.count = 0;
+			this.collisions = new ArrayList<CollisionBox>();
+
+			this.collisions.add( new CollisionBox((int)x+8, (int)y+40, 56, 40, ObjectId.Boss) );
+			//this.collisions.add( new CollisionBox((int)x+40, (int)y, 16, 48, ObjectId.Boss) );
+			
+			
+		}
+
+		public void tick(ArrayList<GameObject> objects) {
+			count++;
+			GameObject player = objects.get(3600);
+			if(count >= 3){
+				
+				this.x+=((player.getX()-this.x)>0)?1:-1;
+				this.y+=((player.getY()-this.y)>0)?1:-1;
+				this.collisions.get(0).x+=((player.getX()-this.x)>0)?1:-1;
+				this.collisions.get(0).y+=((player.getY()-this.y)>0)?1:-1;
+				count = 0;
+			}
+			
+		}
+
+		public void render(Graphics g) {
+			g.drawImage(sprite, (int)x, (int)y, 96, 96, null);
+			
+			if(Window.debug){
+				if(this.getBounds() != null){
+					Graphics2D g2d = (Graphics2D) g;
+					g.setColor(Color.red);
+
+					for (int i = 0; i < getBounds().size(); i++) {
+						if(getBounds().get(i).getId() == ObjectId.Boss)
+							g2d.setColor(Color.MAGENTA);
+						g2d.draw(getBounds().get(i).getBounds());
+					}
+				}
+			}
+		}
+
+		public ArrayList<CollisionBox> getBounds() {
+			return collisions;
+		}
+	}
+	
 }
