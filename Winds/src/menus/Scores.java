9ec91f0a54eefc13259731c5eae2f744150ae029
setokun <s.guardo@live.fr>
2015-07-06 17:10:51 +0200

    Refonte de l'interface Scores et ajout d'une image pour l'obtention d'un trophée

--- Winds/src/menus/Scores.java
+++ Winds/src/menus/Scores.java
@@ -1,7 +1,9 @@
 package menus;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.event.ActionEvent;
@@ -10,8 +12,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -19,8 +20,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.border.SoftBevelBorder;
 import javax.swing.table.DefaultTableModel;
 
 import server.ServerConnection;
@@ -36,35 +36,115 @@ public class Scores extends JPanel {
 	private JLabel title;
 	private JButton jBtnBack, btnUploadMyScores;
 	private Object[][] results = null, resultsTrophies = null;
-	private GroupLayout groupLayout;
-	private JScrollPane scrollPane, scrollPaneTrophies;
-	private int titleMargin;
+	private JScrollPane scrollPaneScores, scrollPaneTrophies;
+	private JPanel north, middle, south, jNorthWest, jNorthEast, jNorthCenter, northSouth;
 	
 	
 	public Scores() {
 				
     	initializeFont();
-		
-    	title = new JLabel("Scores");
-		title.setFont(windsPolice48);
-		titleMargin = 400 - (title.getFontMetrics(title.getFont()).stringWidth(title.getText())/2);
+		this.setLayout(new BorderLayout());
 		
 		initBackButton();
 		initUploadMyScoresButton();
 		
-		groupLayout = new GroupLayout(this);
 		
-		initTableTrophies();
 		initTableScores();
+		initTableTrophies();
 		
-		initHGroup();
-		initVGroup();
+		
+		createNorth();
+		createMiddle();
+		createSouth();
 
+	}
+	
+	private void createNorth() {
+		
+		initTitle();
+		
+		jNorthWest = new JPanel();
+		FlowLayout flNorthWest = new FlowLayout();
+		flNorthWest.setHgap(10);
+		flNorthWest.setVgap(10);
+		jNorthWest.setLayout(flNorthWest);
+		JButton b = new JButton();
+		b.setPreferredSize(new Dimension(131,63));
+		b.setBorder(new SoftBevelBorder(0));
+		b.setBorderPainted(false);
+		b.setContentAreaFilled(false);
+		jNorthWest.add(b);
+		
+		jNorthCenter = new JPanel();
+		FlowLayout flNorthCenter = new FlowLayout();
+		jNorthCenter.setLayout(flNorthCenter);
+		jNorthCenter.add(title);
+		
+		jNorthEast = new JPanel();
+		FlowLayout flNorthEast = new FlowLayout();
+		flNorthEast.setHgap(10);
+		flNorthEast.setVgap(10);
+		jNorthEast.setLayout(flNorthEast);
+		jNorthEast.add(jBtnBack);
+		
+		northSouth = new JPanel();
+		northSouth.setLayout(new BoxLayout(northSouth, BoxLayout.LINE_AXIS));
+		northSouth.add(btnUploadMyScores);
+		
+		north = new JPanel();
+		BorderLayout northLayout = new BorderLayout();
+		north.setLayout(northLayout);
+		north.add(jNorthWest, BorderLayout.WEST);
+		north.add(jNorthCenter, BorderLayout.CENTER);
+		north.add(jNorthEast, BorderLayout.EAST);
+		north.add(northSouth, BorderLayout.SOUTH);
+		this.add(north, BorderLayout.NORTH);
+		
+	}
+	private void createMiddle() {
+		
+		middle = new JPanel();
+		FlowLayout flMiddle = new FlowLayout();
+		flMiddle.setHgap(20);
+		flMiddle.setVgap(20);
+		middle.setLayout(flMiddle);
+		
+		middle.add(scrollPaneScores);
+		this.add(middle, BorderLayout.CENTER);
+		
+	}
+	private void createSouth() {
+		
+		south = new JPanel();
+		FlowLayout flSouth = new FlowLayout();
+		flSouth.setHgap(20);
+		flSouth.setVgap(20);
+		south.setLayout(flSouth);
+		
+		south.add(scrollPaneTrophies);
+		this.add(south, BorderLayout.SOUTH);
+		
+		
+	}
+	
+
+	private void initTitle() {
+		title = new JLabel("Scores");
+		title.setFont(windsPolice48);	
+	}
+	private void initializeFont() {
+		try {
+    		windsPolice18 = Font.createFont(0, getClass().getResourceAsStream("/bubble.ttf")).deriveFont(Font.PLAIN,18F);
+    		windsPolice48 = Font.createFont(0, getClass().getResourceAsStream("/bubble.ttf")).deriveFont(Font.PLAIN,48F);
+		} catch (FontFormatException | IOException e) {
+			windsPolice18 = new Font ("Serif", Font.BOLD, 18);
+    		windsPolice48 = new Font ("Serif", Font.BOLD, 48);
+		}
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private void initTableScores() {
-		scrollPane = new JScrollPane();
+		scrollPaneScores = new JScrollPane();
 		
 		results = Score.getFormattedScores();
 		
@@ -76,7 +156,6 @@ public class Scores extends JPanel {
 		tableScores.setDefaultRenderer(Object.class, new CenterTableCellRenderer());
 		tableScores.setBackground(Color.WHITE);
 		tableScores.setFont(windsPolice18);
-		
 		tableScores.setModel(new DefaultTableModel(
 			results,
 			new String[] {
@@ -102,13 +181,13 @@ public class Scores extends JPanel {
 		for (int i = 0; i < 5; i++) {
 			tableScores.getColumnModel().getColumn(i).setResizable(false);
 		}
-		scrollPane.setViewportView(tableScores);
+		scrollPaneScores.setPreferredSize(new Dimension(760, 170));
+		scrollPaneScores.setViewportView(tableScores);
 	}
-
 	private void initTableTrophies() {
 		scrollPaneTrophies = new JScrollPane();
 		
-		resultsTrophies = Trophy.getTrophies();
+		resultsTrophies = this.getTrophies();
 		
 		tableTrophies = new JTable();
 		tableTrophies.getTableHeader().setBackground(new Color(23,182,255));
@@ -121,7 +200,7 @@ public class Scores extends JPanel {
 		tableTrophies.setModel(new DefaultTableModel(
 			resultsTrophies,
 			new String[] {
-				"Trophies", "OK"
+				"Trophies", ""
 			}
 		) {
 			private static final long serialVersionUID = 5700292645347825439L;
@@ -132,55 +211,13 @@ public class Scores extends JPanel {
 				return columnEditables[column];
 			}
 		});
-		tableTrophies.getColumnModel().getColumn(0).setPreferredWidth(275);
+		tableTrophies.getColumnModel().getColumn(0).setPreferredWidth(500);
 		tableTrophies.getColumnModel().getColumn(1).setResizable(false);
-		tableTrophies.getColumnModel().getColumn(1).setPreferredWidth(50);
+		tableTrophies.getColumnModel().getColumn(1).setPreferredWidth(25);
+		new ButtonColumn(tableTrophies, null, 1);
+		scrollPaneTrophies.setPreferredSize(new Dimension(700, 145));
 		scrollPaneTrophies.setViewportView(tableTrophies);
-		this.setLayout(groupLayout);
-	}
-
-	private void initVGroup() {
-		groupLayout.setVerticalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(title)
-								.addGap(12)
-								.addComponent(btnUploadMyScores))
-							.addComponent(jBtnBack, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE))
-						.addGap(18)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
-						.addGap(34)
-						.addComponent(scrollPaneTrophies, GroupLayout.PREFERRED_SIZE, 141, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(110, Short.MAX_VALUE))
-			);
-	}
-
-	private void initHGroup() {
-		groupLayout.setHorizontalGroup(
-				groupLayout.createParallelGroup(Alignment.TRAILING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 780, Short.MAX_VALUE))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(titleMargin)
-								.addComponent(title)
-								.addPreferredGap(ComponentPlacement.RELATED, 199, Short.MAX_VALUE)
-								.addComponent(jBtnBack, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(btnUploadMyScores))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(155)
-								.addComponent(scrollPaneTrophies, GroupLayout.PREFERRED_SIZE, 500, GroupLayout.PREFERRED_SIZE)))
-						.addContainerGap())
-			);
-	}
-
+	}	
 	private void initUploadMyScoresButton() {
 		btnUploadMyScores = new JButton();
 		btnUploadMyScores.setIcon(new javax.swing.ImageIcon("resources/Buttons/UploadMyScores.png"));
@@ -204,7 +241,6 @@ public class Scores extends JPanel {
 			public void mouseClicked(MouseEvent e) {}
 		});
 	}
-
 	private void initBackButton() {
 		jBtnBack = new JButton();
 		jBtnBack.setIcon(new javax.swing.ImageIcon("resources/Buttons/Back.png"));
@@ -230,37 +266,28 @@ public class Scores extends JPanel {
 		});
 	}
 
-	private void initializeFont() {
-		try {
-    		windsPolice18 = Font.createFont(0, getClass().getResourceAsStream("/bubble.ttf")).deriveFont(Font.PLAIN,18F);
-    		windsPolice48 = Font.createFont(0, getClass().getResourceAsStream("/bubble.ttf")).deriveFont(Font.PLAIN,48F);
-		} catch (FontFormatException | IOException e) {
-			windsPolice18 = new Font ("Serif", Font.BOLD, 18);
-    		windsPolice48 = new Font ("Serif", Font.BOLD, 48);
-		}
-	}
-
 	protected void btnUploadMyScoresActionPerformed(ActionEvent evt) {
 		if(ServerConnection.uploadScores(Score.getLocalScores())){
 			JOptionPane.showMessageDialog(null, "Scores uploaded");
 		}
 	}
-
 	protected void jBtnBackActionPerformed(ActionEvent evt) {
 		Window.resize(new Dimension(800, 550));
 		Window.affect(new MainMenu());
 		
 	}
-
-	public class CenterTableCellRenderer extends DefaultTableCellRenderer {
-		private static final long serialVersionUID = 8632281077361047347L;
-
-		public CenterTableCellRenderer() {
-	        setHorizontalAlignment(CENTER);
-	        setVerticalAlignment(CENTER);
-	    }
+	
+	private Object[][] getTrophies(){
+		Object[][] listTrophies = Trophy.getTrophies();
+		Object[][] listTrophiesToDisplay = null;
+		if(listTrophies != null){
+			listTrophiesToDisplay = new Object[listTrophies.length][2];
+			for(int i=0; i<listTrophies.length; i++){
+				listTrophiesToDisplay[i][0] = listTrophies[i][0];
+				listTrophiesToDisplay[i][1] = listTrophies[i][1].equals("ok")? new ImageIcon("resources/tick.png"):null;
+			}
+		}
+		return listTrophiesToDisplay;
 	}
-	
-	
 	
 }
