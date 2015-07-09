1033e1786febfec026e46ee62e09482c741adb43
setokun <s.guardo@live.fr>
2015-07-09 14:36:19 +0200

    Refactoring complet de la classe Player

--- Winds/src/core/Player.java
+++ Winds/src/core/Player.java
@@ -50,7 +50,6 @@ public class Player extends GameObject{
 		gravity = 0.03f;
 	}
 	
-	@Override
 	public void tick(ArrayList<GameObject> objects) {
 		x += velX; 
 		y += velY + .005f;
@@ -78,213 +77,37 @@ public class Player extends GameObject{
 		for(int i = 0; i<handler.objects.size(); i++){
 			GameObject tempObject = handler.objects.get(i);
 			
-			
-			//if(Math.abs(tempObject.getY() - this.getY()) > 512 && Math.abs(tempObject.getY() - this.getY()) > 256) i+=4;
 			if(Math.abs(tempObject.getX() - this.getX()) > 128) continue;
 			if(Math.abs(tempObject.getY() - this.getY()) > 128)	continue;
+			if(tempObject.getId() == ObjectId.Player) continue;
+			if(tempObject.getBounds() == null) continue;
 			
 			falling = true;
 			
-			if(tempObject.getId() == ObjectId.Player)
-				continue;
-			
-			if(tempObject.getBounds() == null)
-				continue;
-			
 			for (int j = 0; j < tempObject.getBounds().size(); j++) {
 				if(tempObject.getBounds().get(j).getId() == ObjectId.Block){
-					
-					//////////////////////// TOP \\\\\\\\\\\\\\\\\\\\\\\\
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height -4;
-						velY = -(this.getVelY()/4);
-					}
-					
-					//////////////////////// BOTTOM \\\\\\\\\\\\\\\\\\\\\\\\
-					if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).getBounds().y - 60;
-						
-						if(Math.abs(this.getVelY()) < 0.4f){ velY = 0; unsetGravity(); }
-						else{ velY = -(this.getVelY()/1.5f); }
-						
-						if(Math.abs(this.getVelX()) < 0.3f){ velX = 0; }
-						else{ velX = this.getVelX()/1.2f; }
-						
-						falling = false;
-					}
-					else{
-						falling = true;
-						if(timeElapsed % 60 == 0){ resetGravity(); }
-					}
-					
-					//////////////////////// RIGHT \\\\\\\\\\\\\\\\\\\\\\\\
-					if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
-						x = tempObject.getBounds().get(j).x - 60;
-						velX = -(this.getVelX()/2);
-					}
-					
-					//////////////////////// LEFT \\\\\\\\\\\\\\\\\\\\\\\\
-					if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
-						x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).width -4;
-						velX = -(this.getVelX()/2);
-					}
+					actionBlock(tempObject, j);
 				}
-				
 				else if(tempObject.getBounds().get(j).getId() == ObjectId.DangerousBlock 
-						|| tempObject.getBounds().get(j).getId() == ObjectId.Enemy){
-					
-						// TOP
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height;
-						velY = 1.5f;
-						velX = this.getVelX()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// BOTTOM
-					if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).y - 64;
-						velY = -2f;
-						velX = this.getVelX()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// RIGHT
-					if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
-						x = tempObject.getBounds().get(j).x - 64;
-						velX = -1.5f;
-						velY = this.getVelY()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// LEFT
-					if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
-						x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).getBounds().width;
-						velX = 1.5f;
-						velY = this.getVelY()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					
-					
+					 || tempObject.getBounds().get(j).getId() == ObjectId.Enemy){
+					actionDangerousBlockOrEnemy(tempObject, j);
 				}
 				else if(tempObject.getBounds().get(j).getId() == ObjectId.Boss){
-					// TOP
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height;
-						velY = 1.5f;
-						velX = this.getVelX()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// BOTTOM
-					if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
-						y = tempObject.getBounds().get(j).y - 64;
-						velY = -2f;
-						velX = this.getVelX()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// RIGHT
-					if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
-						x = tempObject.getBounds().get(j).x - 64;
-						velX = -1.5f;
-						velY = this.getVelY()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
-					// LEFT
-					if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
-						x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).getBounds().width;
-						velX = 1.5f;
-						velY = this.getVelY()/4;
-						if(!Game.isFinished()){
-							if(!this.isHighlander()) this.getHurt();
-						}
-					}
+					actionBoss(tempObject, j);
 				}
 				else if(tempObject.getBounds().get(j).getId() == ObjectId.Collectable){
-
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
-					|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
-					|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
-					|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
-						CollectableId cid = ((Collectable) tempObject).getCollectableId();
-						
-						if(cid == CollectableId.coin){
-							AudioPlayer.playSfx("piece");
-							Game.score.setNbItems(Game.score.getNbItems()+1);
-						}
-						else if(cid == CollectableId.life){
-							AudioPlayer.playSfx("1up");
-							Game.score.setNbItems(Game.score.getNbItems()+10);
-						    this.life++;
-						    if(this.life>5)
-						    	this.life = 5;
-						}
-						else if(cid == CollectableId.honey){
-							AudioPlayer.playSfx("honey");
-							Game.score.setNbItems(Game.score.getNbItems()+2);
-						}
-						handler.removeObject(tempObject);
-					}
+					actionCollectable(tempObject, j);
 				}
 				else if(tempObject.getBounds().get(j).getId() == ObjectId.Arrival){
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
-					|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
-					|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
-					|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
-						if(!Game.isFinished()){
-							velX = velX / 10;
-							Game.setFinished();
-							Game.bgMusic.stop();
-							Game.bgMusic = new AudioPlayer("/resources/musics/victory.mp3", false);
-							Game.bgMusic.play();
-						}
-						
-					}
+					actionArrival(tempObject, j);
 				}
 				else if(tempObject.getBounds().get(j).getId() == ObjectId.Blower){
-					if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
-							|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
-							|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
-							|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
-						
-						if(((Blower)tempObject).getDirection() == Direction.down) Game.player.setVelY(Math.abs(Game.player.getVelY()) +0.2f);
-						if(((Blower)tempObject).getDirection() == Direction.up) Game.player.setVelY(-Math.abs(Game.player.getVelY()) -0.2f);
-						if(((Blower)tempObject).getDirection() == Direction.left) Game.player.setVelX(-Math.abs(Game.player.getVelX()) -0.2f);
-						if(((Blower)tempObject).getDirection() == Direction.right) Game.player.setVelX(Math.abs(Game.player.getVelX()) +0.2f);
-					}
+					actionBlower(tempObject, j);
 				}
-				
 			}
 		}
 		
-		// 4 instructions to prevent the bubble from going outside the playground
-		if(this.getX() <= 0){
-			this.setX(this.getX() + 2);
-			velX = -(this.getVelX()/2);
-		}
-		if(this.getY() <= 0){
-			this.setY(this.getY() + 2);
-			velY = -(this.getVelY()/2);
-		}
-		
-		if(this.getX() + 72 >= 60*128){
-			this.setX(this.getX() - 2);
-			velX = -(this.getVelX()/2);
-		}
-		if(this.getY() + 92 >= 60*128){
-			this.setY(this.getY() - 2);
-			velY = -(this.getVelY()/2);
-		}
+		preventFromExitPlayground();
 	}
 
 	public void render(Graphics g) {
@@ -315,9 +138,6 @@ public class Player extends GameObject{
 	public Rectangle getBoundsLeft() {
 		return new Rectangle((int)x +4, (int)y+8, (int)8, (int)height-16);
 	}
-
-
-	@Override
 	public ArrayList<CollisionBox> getBounds() {
 		return null;
 	}
@@ -341,6 +161,187 @@ public class Player extends GameObject{
 	
 	public boolean isHighlander(){
 		return invincibleTime > 0;
+	}
+	
+	private void preventFromExitPlayground() {
+		if(this.getX() <= 0){
+			this.setX(this.getX() + 2);
+			velX = -(this.getVelX()/2);
+		}
+		if(this.getY() <= 0){
+			this.setY(this.getY() + 2);
+			velY = -(this.getVelY()/2);
+		}
+		
+		if(this.getX() + 72 >= 60*128){
+			this.setX(this.getX() - 2);
+			velX = -(this.getVelX()/2);
+		}
+		if(this.getY() + 92 >= 60*128){
+			this.setY(this.getY() - 2);
+			velY = -(this.getVelY()/2);
+		}
+	}
+	
+	private void actionArrival(GameObject tempObject, int j) {
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
+				|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
+			if(!Game.isFinished()){
+				velX = velX / 10;
+				Game.setFinished();
+				Game.bgMusic.stop();
+				Game.bgMusic = new AudioPlayer("/resources/musics/victory.mp3", false);
+				Game.bgMusic.play();
+			}
+		}
+	}
+	private void actionBlower(GameObject tempObject, int j) {
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
+				|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
+			
+			if(((Blower)tempObject).getDirection() == Direction.down) Game.player.setVelY(Math.abs(Game.player.getVelY()) +0.2f);
+			if(((Blower)tempObject).getDirection() == Direction.up) Game.player.setVelY(-Math.abs(Game.player.getVelY()) -0.2f);
+			if(((Blower)tempObject).getDirection() == Direction.left) Game.player.setVelX(-Math.abs(Game.player.getVelX()) -0.2f);
+			if(((Blower)tempObject).getDirection() == Direction.right) Game.player.setVelX(Math.abs(Game.player.getVelX()) +0.2f);
+		}
+	}
+	private void actionCollectable(GameObject tempObject, int j) {
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds()) 
+				|| getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())
+				|| getBoundsLeft().intersects(tempObject.getBounds().get(j))){
+					CollectableId cid = ((Collectable) tempObject).getCollectableId();
+					
+					if(cid == CollectableId.coin){
+						AudioPlayer.playSfx("piece");
+						Game.score.setNbItems(Game.score.getNbItems()+1);
+					}
+					else if(cid == CollectableId.life){
+						AudioPlayer.playSfx("1up");
+						Game.score.setNbItems(Game.score.getNbItems()+10);
+					    this.life++;
+					    if(this.life>5)
+					    	this.life = 5;
+					}
+					else if(cid == CollectableId.honey){
+						AudioPlayer.playSfx("honey");
+						Game.score.setNbItems(Game.score.getNbItems()+2);
+					}
+					handler.removeObject(tempObject);
+				}
+	}
+	private void actionBoss(GameObject tempObject, int j) {
+		// TOP
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height;
+			velY = 1.5f;
+			velX = this.getVelX()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// BOTTOM
+		if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).y - 64;
+			velY = -2f;
+			velX = this.getVelX()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// RIGHT
+		if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
+			x = tempObject.getBounds().get(j).x - 64;
+			velX = -1.5f;
+			velY = this.getVelY()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// LEFT
+		if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
+			x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).getBounds().width;
+			velX = 1.5f;
+			velY = this.getVelY()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+	}
+	private void actionDangerousBlockOrEnemy(GameObject tempObject, int j) {
+		// TOP
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height;
+			velY = 1.5f;
+			velX = this.getVelX()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// BOTTOM
+		if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).y - 64;
+			velY = -2f;
+			velX = this.getVelX()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// RIGHT
+		if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
+			x = tempObject.getBounds().get(j).x - 64;
+			velX = -1.5f;
+			velY = this.getVelY()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+		// LEFT
+		if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
+			x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).getBounds().width;
+			velX = 1.5f;
+			velY = this.getVelY()/4;
+			if(!Game.isFinished()){
+				if(!this.isHighlander()) this.getHurt();
+			}
+		}
+	}
+	private void actionBlock(GameObject tempObject, int j) {
+		////////////////////////TOP \\\\\\\\\\\\\\\\\\\\\\\\
+		if(getBoundsTop().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).y + tempObject.getBounds().get(j).height -4;
+			velY = -(this.getVelY()/4);
+		}
+		//////////////////////// BOTTOM \\\\\\\\\\\\\\\\\\\\\\\\
+		if(getBoundsBottom().intersects(tempObject.getBounds().get(j).getBounds())){
+			y = tempObject.getBounds().get(j).getBounds().y - 60;
+			
+			if(Math.abs(this.getVelY()) < 0.4f){ velY = 0; unsetGravity(); }
+			else{ velY = -(this.getVelY()/1.5f); }
+			
+			if(Math.abs(this.getVelX()) < 0.3f){ velX = 0; }
+			else{ velX = this.getVelX()/1.2f; }
+			
+			falling = false;
+		}
+		else{
+			falling = true;
+			if(timeElapsed % 60 == 0){ resetGravity(); }
+		}
+		//////////////////////// RIGHT \\\\\\\\\\\\\\\\\\\\\\\\
+		if(getBoundsRight().intersects(tempObject.getBounds().get(j).getBounds())){	
+			x = tempObject.getBounds().get(j).x - 60;
+			velX = -(this.getVelX()/2);
+		}
+		//////////////////////// LEFT \\\\\\\\\\\\\\\\\\\\\\\\
+		if(getBoundsLeft().intersects(tempObject.getBounds().get(j))){ 	
+			x = tempObject.getBounds().get(j).x + tempObject.getBounds().get(j).width -4;
+			velX = -(this.getVelX()/2);
+		}
 	}
 	
 }
