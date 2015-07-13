569ef57be390282712c0307f20362104cedcf246
Damien08300 <damien.deloche@gmail.com>
2015-07-13 22:43:34 +0200

    Nettoyage et complément de Window

--- Winds/src/display/Window.java
+++ Winds/src/display/Window.java
@@ -12,7 +12,9 @@ import menus.MainMenu;
 import account.Profile;
 import database.DBClass;
 
-
+/**
+ * Class used to centralize and hold all JPanel and Canvas of the Winds application.
+ */
 public class Window {
 	public static final Dimension DIM_STANDARD = new Dimension(800, 550);
 	public static final Dimension DIM_EDITOR = new Dimension(1024, 600);
@@ -22,14 +24,14 @@ public class Window {
 	private static JFrame main;
 	
 	/**
-	 * Constructor of Window Class, creates a JFrame which will hold all JPanel and Canvas of the Program
+	 * Forbid to instantiate by the default way.
 	 */
 	private Window(){}
 	
 	/**
 	 * affects a new component to the main JFrame and resizes it 
 	 * @param c the component to affect into the JFrame
-	 * @param dim the new dimensionz
+	 * @param dim the new dimension
 	 */
 	public static void affect(Component c, Dimension dim){
 		if(main.getContentPane().getComponentCount() > 0) main.getContentPane().removeAll();
@@ -46,7 +48,10 @@ public class Window {
 	public static JFrame getFrame(){
 		return main;
 	}
-	
+	/**
+	 * Builds the Winds main frame.<br>
+	 * This method must be called only by the splash screen.
+	 */
 	public static void init(){
 		main = new JFrame("Winds");
 		main.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -62,9 +67,12 @@ public class Window {
 		
 		try {
 			main.setIconImage(ImageIO.read(Window.class.getClass().getResource("/resources/collectables/bubulle.png")));
-		} catch (IOException e) { e.printStackTrace(); }
+		} catch (IOException e) {}
 	}
-	
+	/**
+	 * Displays the main frame.<br>
+	 * This method must be called only by the splash screen.
+	 */
 	public static void start(){
 		affect( Profile.getCurrentPlayer() == null ? new Login() : new MainMenu(), DIM_STANDARD);
 	}
