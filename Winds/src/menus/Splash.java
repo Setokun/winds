6bbbf82bbeae7aaf10a5afb9e04c6397e2f5973a
Damien08300 <damien.deloche@gmail.com>
2015-07-14 00:59:26 +0200

    Complément du splash screen

--- Winds/src/menus/Splash.java
+++ Winds/src/menus/Splash.java
@@ -3,87 +3,103 @@ package menus;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.io.IOException;
+import java.awt.Toolkit;
 import java.lang.reflect.Method;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import account.Profile;
 import addon.AddonManager;
 import display.Window;
 
 
+/**
+ * Class used to display the Winds splash <br>
+ * screen during the loadings of main items.
+ */
 public class Splash extends JFrame {
 	private static final long serialVersionUID = -7831305789171898879L;
+	private JLabel lblLoad;
 	
-	private ImageIcon image;
-	private JLabel lblTop, lblBottom;
-	
+	/**
+	 * Instantiates a new Winds splash screen.
+	 */
 	public Splash(){
 		initStructure();
 		initFrame();
 		EventQueue.invokeLater(()->{ initLoad(); });
 	}
 	
+	//region Methods 
+	/**
+	 * Initializes the splash screen's structure.
+	 */
 	private void initStructure(){
-		JPanel pnl = new JPanel();
-		
-		lblTop = new JLabel("Welcome  into  Winds  world");
-		try { lblTop.setFont( Font.createFont(0, getClass()
+		JLabel lblBack = new JLabel("Welcome into Winds");
+		lblBack.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblBack.setVerticalTextPosition(SwingConstants.CENTER);
+		lblBack.setIcon(new ImageIcon(getClass().getResource("/resources/background/splash.jpg")));
+		try { lblBack.setFont( Font.createFont(0, getClass()
     			    .getResourceAsStream("/resources/font/bubble.ttf"))
-    			    .deriveFont(Font.PLAIN,23F) );
+    			    .deriveFont(Font.PLAIN,34F) );
 		} catch (Exception e) {
-			lblTop.setFont( new Font("Serif", Font.BOLD, 23) );
+			lblBack.setFont( new Font("Serif", Font.BOLD, 34) );
 		}
-		pnl.add(lblTop, BorderLayout.NORTH);
+		add(lblBack, BorderLayout.CENTER);
+				
+		lblLoad = new JLabel("Please wait the load of files. Starting soon.");
+		lblLoad.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLoad.setVerticalAlignment(SwingConstants.TOP);
+		add(lblLoad, BorderLayout.SOUTH);
 		
-		
-		try {
-			image = new ImageIcon( getClass().getResource("/resources/background/splash.jpg") );
-		} catch (Exception e){}
-		if(image != null) pnl.add(new JLabel(image), BorderLayout.CENTER);
-		
-		lblBottom = new JLabel("Please wait the loading files.");
-		pnl.add(lblBottom, BorderLayout.SOUTH);
-		
-		add(pnl);		
 	}
+	/**
+	 * Initializes the splash screen's frame.
+	 */
 	private void initFrame() {
-		try {
-			setIconImage( ImageIO.read(Window.class.getClass().getResource("/resources/bubble.png")) );
-		} catch (IOException e) {}
-		
+		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/bubble.png")));
 		setSize(Window.DIM_SPLASH);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		setUndecorated(true);
 		setAlwaysOnTop(true);
 		toFront();
+		pack();
 		setVisible(true);
 	}
+	/**
+	 * Loads the main items of Winds.
+	 */
 	private void initLoad(){
 		launchMethod("Loading GUI", Window.class, "init");
 		launchMethod("Loading profile", Profile.class, "init");
 		launchMethod("Loading add-ons", AddonManager.class, "init");
-		launchMethod("Starting game", Window.class, "start");
+		launchMethod("Starting", Window.class, "start");
 		dispose();		
 	}
-	
+	/**
+	 * Launch the method of a main item of Winds.
+	 * @param txt The text to display in the splash screen
+	 * @param cls The class of the method to launch 
+	 * @param methodName The method's name to launch
+	 */
 	private void launchMethod(String txt, Class<?> cls, String methodName) {
 		try {
-			lblBottom.setText(txt);
+			lblLoad.setText(txt);
 			Method m = cls.getDeclaredMethod(methodName);
 			m.invoke(null);
-			lblBottom.repaint();
 		} catch (Exception e) {}
 	}
+	//endregion	
 	
-	
-	//region Application launcher
+	//region Application launcher 
+	/**
+	 * Main method to launch the Winds application.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(()->{ new Splash(); });
 	}
