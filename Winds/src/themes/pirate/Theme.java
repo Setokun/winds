913bc8ff7b19e2164de65c47e84b749610124d0b
setokun <s.guardo@live.fr>
2015-07-16 16:16:46 +0200

    Modifications dans le cadre de la gestion des erreurs liées à l'accès aux scores ou au shop avec un MDP ayant changé depuis la dernière connexion

--- Winds/src/themes/pirate/Theme.java
+++ Winds/src/themes/pirate/Theme.java
@@ -118,7 +118,7 @@ public class Theme extends ThemeBase {
 		compatibility.put(new Point(16,0), new Integer[]{2,3,4,6,10,11,12,13,15,17,21,22,23,24});
 		compatibility.put(new Point(16,1), new Integer[]{2,4,6,16,17,21,22});
 		compatibility.put(new Point(16,2), new Integer[]{3,11,13,14});
-		compatibility.put(new Point(16,3), new Integer[]{2,3,5,6,7,9,10,14,15,16,17,18,19,20,21});
+		compatibility.put(new Point(16,3), new Integer[]{1,2,3,5,6,7,9,10,14,15,16,17,18,19,20,21});
 
 		compatibility.put(new Point(17,0), new Integer[]{7,8,9,14,18});
 		compatibility.put(new Point(17,1), new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24});
